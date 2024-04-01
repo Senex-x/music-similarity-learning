15de31ede0ff3faca1bc12e3c1da31f6cccaba87
V-Senex <junior.07.inator@gmail.com>
2024-04-01 19:15:34 +0300

    Find several similar tracks for a chosen track

--- similarity_learning.py
+++ similarity_learning.py
@@ -18,18 +18,45 @@ class SimilarityLearning:
 
     def __init__(self):
         self.tokenized_music_folder_path = join(dirname(__file__), 'data/music_tokens')
+        self.nn_model = NearestNeighbors()
 
     def learn(self):
-        nn_model = NearestNeighbors()
         samples = []
-
-        music_token_files = listdir(self.tokenized_music_folder_path)
+        music_token_files = listdir(self.tokenized_music_folder_path)[:1000]
         for file in music_token_files:
             samples.append(self.__load_mfcc_vector(file).tolist())
 
-        nn_model.fit(samples)
+        self.nn_model.fit(samples)
 
-        neighbour_data_list = self.__associate_tracks_with_nearest_neighbour(nn_model, music_token_files)
+        neighbour_data_list = self.__find_neighbours_for_track('A Day To Remember - Homesick', music_token_files)
+
+        print(f'Track: {neighbour_data_list[0].origin} is most similar to:')
+        for i, neighbour_data in enumerate(neighbour_data_list):
+            print(f'{i + 1}: {neighbour_data.neighbour} \t // with similarity of: {round(neighbour_data.distance * 100, 2)}%')
+
+        # self.__find_nearest_neighbour_for_each_track(nn_model, music_token_files)
+
+    def __find_neighbours_for_track(self, track_name, token_files, neighbours_amount=10):
+        neighbours = []
+        (distances_output, neighbours_output) = self.nn_model.kneighbors(
+            [self.__load_mfcc_vector(track_name + '.npy').tolist()],
+            n_neighbors=neighbours_amount + 1,
+            return_distance=True)
+
+        for i in range(len(list(neighbours_output[0])))[1:]:
+            distance = distances_output[0][i]
+            neighbour = neighbours_output[0][i]
+
+            neighbours.append(
+                NeighbourData(origin=track_name,
+                              neighbour=self.__trim_extension(token_files[neighbour]),
+                              distance=distance))
+
+        self.__normalize_distances(neighbours)
+        return neighbours
+
+    def __find_nearest_neighbour_for_each_track(self, token_files):
+        neighbour_data_list = self.__associate_tracks_with_nearest_neighbour(token_files)
         self.__normalize_distances(neighbour_data_list)
 
         for neighbour_data in neighbour_data_list:
@@ -42,7 +69,6 @@ class SimilarityLearning:
 
         for neighbour_data in neighbour_data_list:
             max_distance = max(max_distance, neighbour_data.distance)
-
         for neighbour_data in neighbour_data_list:
             neighbour_data.distance = 1 - neighbour_data.distance / max_distance
 
@@ -55,18 +81,16 @@ class SimilarityLearning:
     # Track: A$AP Rocky - Jodye.npy 	 is most similar to: 	 A$AP Rocky - Lord Pretty Flacko Jodye 2 (LPFJ2).npy with similarity of: 	 100.0%
     # Track: A$AP Rocky - Ghetto Symphony (feat. Gunplay & A$AP Ferg).npy 	 is most similar to: 	 Alan Walker - Alone, Pt. II.npy with similarity of: 	 92.71%
 
-    def __associate_tracks_with_nearest_neighbour(self, nn_model, token_files):
+    def __associate_tracks_with_nearest_neighbour(self, token_files):
         neighbours = []
-
         for file in token_files:
-            nearest_neighbours = nn_model.kneighbors([self.__load_mfcc_vector(file).tolist()],
-                                                     n_neighbors=2,
-                                                     return_distance=True)
+            nearest_neighbours = self.nn_model.kneighbors([self.__load_mfcc_vector(file).tolist()],
+                                                          n_neighbors=2,
+                                                          return_distance=True)
             nearest_neighbour = NeighbourData(origin=self.__trim_extension(file),
                                               neighbour=self.__trim_extension(token_files[nearest_neighbours[1][0][1]]),
                                               distance=nearest_neighbours[0][0][1])
             neighbours.append(nearest_neighbour)
-
         return neighbours
 
     def __load_mfcc_vector(self, file_name):
